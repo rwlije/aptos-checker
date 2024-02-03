f9ff8517b5c12fa4491294438f46b875f58d0ef2
rwlije <113174590+rwlije@users.noreply.github.com>
2024-02-03 23:16:08 +0500

    macos compatibility

--- main.py
+++ main.py
@@ -39,5 +39,8 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    try:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    except Exception:
+        pass
     asyncio.run(main())
