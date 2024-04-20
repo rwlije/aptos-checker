fb4adad1d4081b01d0302848a34bc4716ec3d623
rwlije <113174590+rwlije@users.noreply.github.com>
2024-04-20 18:36:12 +0500

    new stats

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from core.constants import OATS
 from itertools import cycle
 from utils.log import log
 import pandas as pd
+import numpy as np
 import asyncio
 import httpx
 import numpy
@@ -13,7 +14,7 @@ import numpy
 async def start_work(semaphore, seed_phrase, session, client):
     async with semaphore:
         result = await client.get_all_info(seed_phrase, session)
-        await asyncio.sleep(2)
+        await asyncio.sleep(2.5)
         return result
 
 
@@ -48,21 +49,29 @@ async def main():
     tasks = [asyncio.create_task(start_work(semaphore, seed_phrase, session, client)) for seed_phrase, session in
              zip(seed_phrases, cycle(sessions))]
     results = await asyncio.gather(*tasks)
-    succeeded_wallets = [result for result in results if isinstance(result, list)]
+    succeeded_wallets = np.array([result for result in results if isinstance(result, list)])
     failed_wallets = [result for result in results if not (isinstance(result, list))]
 
     try:
+        amount_of_completed_quests = np.sum(succeeded_wallets[:, -len(OATS):].astype(int), axis=1)
+        n_completed_quests = np.zeros((succeeded_wallets.shape[0], len(OATS)), dtype=int)
+
+        for i in range(len(OATS)):
+            n_completed_quests[:, i] = (amount_of_completed_quests >= (i + 1)).astype(int)
+
+        succeeded_wallets = np.column_stack((succeeded_wallets, n_completed_quests))
+        total = np.array([None for _ in range(succeeded_wallets.shape[1])])
+        total[-len(OATS) * 2:] = np.sum(succeeded_wallets[:, -len(OATS) * 2:].astype(int), axis=0)
+        succeeded_wallets = np.vstack((succeeded_wallets, total))
         df = pd.DataFrame(succeeded_wallets, columns=numpy.array(["address", "seed phrase", "private key", "balance",
                                                                   "transactions", "domain name", "quest 1 oat",
-                                                                  "quest 2 oat", "quest 3 oat", "quest 4 oat"]))
-        df.loc[df.shape[0]] = [None for _ in range(df.shape[1] - len(OATS))] + [int(df[column].sum()) for column in
-                                                                                df.columns[-len(OATS):]]
-        for column in df.columns[-len(OATS):]:
-            df[column] = df[column].astype(int)
-            
+                                                                  "quest 2 oat", "quest 3 oat", "quest 4 oat",
+                                                                  "any 1 quest", "any 2 quests", "any 3 quests",
+                                                                  "all 4 quests"]))
         df.to_csv("files/table.csv", index=False, columns=("address", "seed phrase", "private key", "balance",
                                                            "transactions", "domain name", "quest 1 oat",
-                                                           "quest 2 oat", "quest 3 oat", "quest 4 oat"))
+                                                           "quest 2 oat", "quest 3 oat", "quest 4 oat", "any 1 quest",
+                                                           "any 2 quests", "any 3 quests", "all 4 quests"), na_rep="")
         write_lines("files/unchecked_wallets.txt", "\n".join(failed_wallets))
         log.success("Работа успешно завершена")
 
